e0f60c3bda00ae36fb0d0ba7536cc1fe835e204c
Eslam <eslamelezaby98@gmail.com>
2021-06-10 18:42:48 +0200

    details screen

--- daily_exercises_app_ui/lib/screens/details_screen.dart
+++ daily_exercises_app_ui/lib/screens/details_screen.dart
@@ -1,8 +1,77 @@
+import 'package:daily_exercises_app_ui/widgets/search_bar.dart';
 import 'package:flutter/material.dart';
 
 class DetialsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: size.height * 0.45,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/meditation_bg.png',
+                  ),
+                  fit: BoxFit.fitWidth),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: size.height * 0.05,
+                    ),
+                    Text(
+                      'Meditation',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline4
+                          .copyWith(fontWeight: FontWeight.w900),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "3-10 MIN Course",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    SizedBox(
+                      width: size.width * .6, // it just take 60% of total width
+                      child: Text(
+                        "Live happier and healthier by learning the fundamentals of meditation and mindfulness",
+                      ),
+                    ),
+                    SizedBox(
+                      width: size.width * .5, // it just take the 50% width
+                      child: SearchBar(),
+                    ),
+                    Wrap(
+                      spacing: 20,
+                      runSpacing: 20,
+                      children: [
+                        SessionCard(),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SessionCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
