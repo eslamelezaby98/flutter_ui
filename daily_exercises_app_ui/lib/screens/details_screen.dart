62cbdfb5829f7500baefc108c6005ee08a23de06
Eslam <eslamelezaby98@gmail.com>
2021-06-10 19:03:11 +0200

    Finished details screen

--- daily_exercises_app_ui/lib/screens/details_screen.dart
+++ daily_exercises_app_ui/lib/screens/details_screen.dart
@@ -1,5 +1,7 @@
+import 'package:daily_exercises_app_ui/constants/constants.dart';
 import 'package:daily_exercises_app_ui/widgets/search_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DetialsScreen extends StatelessWidget {
   @override
@@ -55,8 +57,81 @@ class DetialsScreen extends StatelessWidget {
                       spacing: 20,
                       runSpacing: 20,
                       children: [
-                        SessionCard(),
+                        SessionCard(
+                          seassionNum: 1,
+                          isDone: true,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          seassionNum: 2,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          seassionNum: 3,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          seassionNum: 4,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          seassionNum: 5,
+                          press: () {},
+                        ),
+                        SessionCard(
+                          seassionNum: 6,
+                          press: () {},
+                        ),
                       ],
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'Meditation',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline6
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            offset: Offset(0, 17),
+                            blurRadius: 23,
+                            spreadRadius: -13,
+                            color: kShadowColor,
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          SvgPicture.asset(
+                            "assets/icons/Meditation_women_small.svg",
+                          ),
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  "Basic 2",
+                                  style: Theme.of(context).textTheme.subtitle2,
+                                ),
+                                Text("Start your deepen you practice")
+                              ],
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: SvgPicture.asset("assets/icons/Lock.svg"),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
@@ -70,8 +145,68 @@ class DetialsScreen extends StatelessWidget {
 }
 
 class SessionCard extends StatelessWidget {
+  final int seassionNum;
+  final bool isDone;
+  final Function press;
+  const SessionCard({
+    this.seassionNum,
+    this.isDone = false,
+    this.press,
+  });
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(13),
+          child: Container(
+            width: constraints.maxWidth / 2 - 10,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(13),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 17),
+                  blurRadius: 23,
+                  spreadRadius: -13,
+                  color: kShadowColor,
+                ),
+              ],
+            ),
+            child: Material(
+              color: Colors.white,
+              child: InkWell(
+                onTap: press,
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Row(
+                    children: [
+                      Container(
+                        height: 42,
+                        width: 43,
+                        decoration: BoxDecoration(
+                          color: isDone ? kBlueColor : Colors.white,
+                          shape: BoxShape.circle,
+                          border: Border.all(color: kBlueColor),
+                        ),
+                        child: Icon(
+                          Icons.play_arrow,
+                          color: isDone ? Colors.white : kBlueColor,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        "Session $seassionNum",
+                        style: Theme.of(context).textTheme.subtitle2,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
