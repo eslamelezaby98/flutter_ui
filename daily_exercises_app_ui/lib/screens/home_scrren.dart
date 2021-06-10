ef4539ead9678a356ddf932fa9fb11aeaa8e8172
Eslam <eslamelezaby98@gmail.com>
2021-06-10 19:07:56 +0200

    fix spilling morning

--- daily_exercises_app_ui/lib/screens/home_scrren.dart
+++ daily_exercises_app_ui/lib/screens/home_scrren.dart
@@ -43,7 +43,7 @@ class HomeScreen extends StatelessWidget {
                     ),
                   ),
                   Text(
-                    'Good Mornign \nEslam',
+                    'Good Morning \nEslam',
                     style: Theme.of(context)
                         .textTheme
                         .headline4
