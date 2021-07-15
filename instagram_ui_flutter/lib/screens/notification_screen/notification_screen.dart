64e43c128a54473132aff56997feb8150eed2cb6
Eslam Elezaby <eslamelezaby98@gmail.com>
2021-07-15 22:15:08 +0200

    add instgram_ui_flutter

---
+++ instagram_ui_flutter/lib/screens/notification_screen/notification_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotificationsScreen extends StatefulWidget {
+  @override
+  _NotificationsScreenState createState() => _NotificationsScreenState();
+}
+
+class _NotificationsScreenState extends State<NotificationsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+}
